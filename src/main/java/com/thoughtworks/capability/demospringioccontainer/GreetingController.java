10ea73d75ad4d14c1fe8f924ae3955500f118d53
liyuan <823260355@qq.com>
2020-11-08 23:34:33 +0800

    [liyuan] add appContext

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,32 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private ApplicationContext applicationContext;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    public GreetingService getGreetingService() {
+        return applicationContext.getBean(GreetingService.class);
     }
 
     @GetMapping("/greet")
     public String greet() {
+
+        GreetingService greetingService = getGreetingService();
+        System.out.println("Greeting Service: " + greetingService);
+        System.out.println("Greeting Controller: " + this + "\n");
         return greetingService.sayHi();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
 }
