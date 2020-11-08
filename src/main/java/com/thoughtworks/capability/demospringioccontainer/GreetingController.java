c51111bbd3d70fdb5b846b2f0770f77cc995ea86
liyuan <823260355@qq.com>
2020-11-08 23:43:30 +0800

    [liyuan] add lookup annotation

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,22 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    @Lookup
+    public GreetingService getGreetingService() {
+        return null;
     }
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = getGreetingService();
+        System.out.println("Greeting Service: " + greetingService);
+        System.out.println("Greeting Controller: " + this + "\n");
         return greetingService.sayHi();
     }
-
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
